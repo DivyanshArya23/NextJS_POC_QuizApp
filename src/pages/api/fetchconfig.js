44a3bdd0c6d0eabbb79f5eea0db7d69e00e1f1ce
DivyanshArya23 <divyansharya20@gmail.com>
2022-02-04 18:41:39 +0530

    logs added in api

--- src/pages/api/fetchconfig.js
+++ src/pages/api/fetchconfig.js
@@ -1,20 +1,27 @@
 export default function handler(req, res) {
   if (req.method === "GET") {
+    console.log({
+      req: req,
+      api: "fetchconfig",
+      method: req.method,
+    });
+    const payload = [
+      {
+        id: 1,
+        maxValue: 10,
+        noq: 2,
+        operators: ["+", "-", "/", "*"],
+      },
+      {
+        id: 2,
+        maxValue: 10,
+        noq: 20,
+        operators: ["+", "-", "/", "*"],
+      },
+    ];
+    console.log("returning from fetchconfig api with payload :", payload);
     res.status(200).json({
-      configData: [
-        {
-          id: 1,
-          maxValue: 10,
-          noq: 2,
-          operators: ["+", "-", "/", "*"],
-        },
-        {
-          id: 2,
-          maxValue: 10,
-          noq: 20,
-          operators: ["+", "-", "/", "*"],
-        },
-      ],
+      configData: payload,
     });
   }
 }
